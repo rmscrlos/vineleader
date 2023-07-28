6d3267ef14ac1e06aeb11f1ed77f44fb068562dd
Carlos Ramos <carlosramos@carloss-air.lan>
2023-07-28 16:48:09 -0400

    app init

---
+++ tailwind.config.js
@@ -0,0 +1,45 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: [
+		"./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+	],
+	theme: {
+		extend: {
+			backgroundImage: {
+				"gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+				"gradient-conic":
+					"conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+			},
+			colors: {
+				dodgerBlue: "#1F8AFF",
+				backgroundWhite: "#f5f5f5",
+				darkBlueGray: "#213442",
+			},
+		},
+		screens: {
+			xxl: { max: "1535px" },
+			// => @media (max-width: 1535px) { ... }
+
+			xl: { max: "1279px" },
+			// => @media (max-width: 1279px) { ... }
+
+			lg: { max: "1023px" },
+			// => @media (max-width: 1023px) { ... }
+
+			md: { max: "768px" },
+			// => @media (max-width: 767px) { ... }
+
+			sm: { max: "639px" },
+			// => @media (max-width: 639px) { ... }
+
+			minMd: "768px",
+			// => @media (min-width: 768px) { ... }
+
+			minSm: "640px",
+			// => @media (min-width: 640px) { ... }
+		},
+	},
+	plugins: [],
+};
