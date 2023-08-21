7669504c8fe05641e1f834877700886f58e28760
Carlos Ramos <carlosramos@Carloss-MacBook-Air.local>
2023-08-21 16:10:38 -0400

    made vine logo clickable

--- src/components/VineLeaderLogo.tsx
+++ src/components/VineLeaderLogo.tsx
@@ -1,12 +1,15 @@
 import { cn } from "@/lib/utils";
+import Link from "next/link";
 
 const VineLeaderLogo = ({
 	className,
 }: React.HTMLAttributes<HTMLDivElement>) => {
 	return (
-		<span className={cn(className, "text-3xl font-geo")}>
-			vine <span className="text-blueaccent-1 font-[600]">leader</span>
-		</span>
+		<Link href="/">
+			<span className={cn(className, "text-3xl font-geo")}>
+				vine <span className="text-blueaccent-1 font-[600]">leader</span>
+			</span>
+		</Link>
 	);
 };
 
